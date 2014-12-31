1c295bcb97b6e992cba62ca3cd7d9804a3e996ca
unknown <markmans@SEA-1201041719.ant.amazon.com>
2014-12-30 16:18:50 -0800

    Diable Ergo emacs and set theme to Cobalt and font to Inconsolata

---
+++ custom-init.el
@@ -0,0 +1,16 @@
+(cond
+ ((string-equal system-type "windows-nt") ; Microsoft Windows
+  (progn
+    (let (rootpath)
+      (setq rootpath
+            (concat
+             (getenv "HOMESHARE")
+             "/Code/emacs-config/"
+             ))
+      (message rootpath)
+      (load (concat rootpath "disable-ergoemacs.el"))
+      (load (concat rootpath "./look-and-feel.el"))
+      )
+    )
+  ))
+
---
+++ look-and-feel.el
@@ -0,0 +1,8 @@
+;; Color Themes
+(require 'color-theme)
+(color-theme-initialize)
+(load-file "~/.emacs.d/themes/color-theme-cobalt.el")
+(color-theme-cobalt)
+
+;; Setup the default font
+(set-default-font "Inconsolata-12")
