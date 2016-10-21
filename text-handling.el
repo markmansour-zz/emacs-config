ad8e18580e56745ce3570364a0f0e0e725a9692a
Mark Mansour <markmans@amazon.com>
2016-10-21 14:41:43 -0700

    added find-next-unsafe-char and unfill-paragraph

--- text-handling.el
+++ text-handling.el
@@ -17,3 +17,47 @@
 
 ;; copy highlighted text to the clipboard
 (setq mouse-drag-copy-region t)
+
+;; this function will allow you to find characters your current coding system cannot encode
+;; https://www.emacswiki.org/emacs/FindingNonAsciiCharacters
+(defun find-next-unsafe-char (&optional coding-system)
+  "Find the next character in the buffer that cannot be encoded by
+coding-system. If coding-system is unspecified, default to the coding
+system that would be used to save this buffer. With prefix argument,
+prompt the user for a coding system."
+  (interactive "Zcoding-system: ")
+  (if (stringp coding-system) (setq coding-system (intern coding-system)))
+  (if coding-system nil
+    (setq coding-system
+	  (or save-buffer-coding-system buffer-file-coding-system)))
+  (let ((found nil) (char nil) (csets nil) (safe nil))
+    (setq safe (coding-system-get coding-system 'safe-chars))
+    ;; some systems merely specify the charsets as ones they can encode:
+    (setq csets (coding-system-get coding-system 'safe-charsets))
+    (save-excursion
+      ;;(message "zoom to <")
+      (let ((end  (point-max))
+	    (here (point    ))
+	    (char  nil))
+	(while (and (< here end) (not found))
+	  (setq char (char-after here))
+	  (if (or (eq safe t)
+		  (< char ?\177)
+		  (and safe  (aref safe char))
+		  (and csets (memq (char-charset char) csets)))
+	      nil ;; safe char, noop
+	    (setq found (cons here char)))
+	  (setq here (1+ here))) ))
+    (and found (goto-char (1+ (car found))))
+    found))
+
+
+;;; https://www.emacswiki.org/emacs/UnfillParagraphcon
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+	;; This would override `fill-column' if it's an integer.
+	(emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
